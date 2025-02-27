a76eb3c1b0064345c87f751d2502f36bf4865371
Gustavo Ulian Zanardi <134564393+GustavoZanardi15@users.noreply.github.com>
2025-02-27 15:36:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let yBolinha = 200;
 let diametro = 30;
 let vxBolinha = 5;
 let vyBolinha = 5;
-let raio = diametro /2;
+let raio = diametro / 2;
 let xRaquete = 10;
 let yRaquete = 150;
 let comprimentoRaquete = 10;
@@ -11,46 +11,53 @@ let larguraRaquete = 100;
 
 function setup() {
     createCanvas(600, 400);
+}
 
-  }
-  
-  function draw() {
+function draw() {
     background(0);
+    
     CriarBolinha();
+    VerificarColisaoBorda();
     MoverRaquete();
+    VerificarColisaoRaquete();
+}
 
-  function CriarBolinha(){
-    circle(xBolinha,yBolinha,diametro);
+
+function CriarBolinha() {
+    circle(xBolinha, yBolinha, diametro);
     xBolinha += vxBolinha;
     yBolinha += vyBolinha;
-  }
-   
+}
 
-    if(xBolinha + raio > 600 || xBolinha -raio < 0){
+
+function VerificarColisaoBorda() {
+    if (xBolinha + raio > width || xBolinha - raio < 0) {
         vxBolinha *= -1;
-
     }
-    if(yBolinha + raio > 400 || yBolinha -raio < 0){
+    if (yBolinha + raio > height || yBolinha - raio < 0) {
         vyBolinha *= -1;
-
     }
+}
 
 
-    function MoverRaquete(){
-    rect(xRaquete,yRaquete, comprimentoRaquete, larguraRaquete);
-    
-    if(keyIsDown(UP_ARROW) === true){
+function MoverRaquete() {
+    rect(xRaquete, yRaquete, comprimentoRaquete, larguraRaquete);
+
+    if (keyIsDown(UP_ARROW)) {
         yRaquete -= 10;
     }
-
-    if(keyIsDown(DOWN_ARROW) === true){
+    if (keyIsDown(DOWN_ARROW)) {
         yRaquete += 10;
     }
+}
 
+
+function VerificarColisaoRaquete() {
+    if (
+        xBolinha - raio < xRaquete + comprimentoRaquete && 
+        yBolinha > yRaquete && 
+        yBolinha < yRaquete + larguraRaquete 
+    ) {
+        vxBolinha *= -1; 
     }
-  }
-
-  
-
-
-  
+}
