98e31a9ca895c3786967ed0514d2daaa55cb2c83
Gustavo Ulian Zanardi <134564393+GustavoZanardi15@users.noreply.github.com>
2025-02-27 21:44:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let xRaquete = 10;
 let yRaquete = 150;
 let comprimentoRaquete = 10;
 let larguraRaquete = 100;
+let meusPontos = 0;  
+let pontosDoOponente = 0;  
 
 function setup() {
     createCanvas(600, 400);
@@ -15,13 +17,13 @@ function setup() {
 
 function draw() {
     background(0);
-    
+
     CriarBolinha();
     VerificarColisaoBorda();
     MoverRaquete();
     VerificarColisaoRaquete();
+    incluiPlacar();
 }
-
 
 function CriarBolinha() {
     circle(xBolinha, yBolinha, diametro);
@@ -29,35 +31,49 @@ function CriarBolinha() {
     yBolinha += vyBolinha;
 }
 
-
 function VerificarColisaoBorda() {
-    if (xBolinha + raio > width || xBolinha - raio < 0) {
-        vxBolinha *= -1;
+    if (xBolinha + raio > width) {
+        pontosDoOponente++;
+        resetBolinha();
+    }
+    if (xBolinha - raio < 0) {
+        meusPontos++;
+        resetBolinha();
     }
     if (yBolinha + raio > height || yBolinha - raio < 0) {
-        vyBolinha *= -1;
+        vyBolinha *= -1; 
     }
 }
-
 
 function MoverRaquete() {
     rect(xRaquete, yRaquete, comprimentoRaquete, larguraRaquete);
 
-    if (keyIsDown(UP_ARROW)) {
-        yRaquete -= 10;
+    if (keyIsDown(UP_ARROW) && yRaquete > 0) {
+        yRaquete -= 10; 
     }
-    if (keyIsDown(DOWN_ARROW)) {
-        yRaquete += 10;
+    if (keyIsDown(DOWN_ARROW) && yRaquete < height - larguraRaquete) {
+        yRaquete += 10; 
     }
 }
 
-
 function VerificarColisaoRaquete() {
-    if (
-        xBolinha - raio < xRaquete + comprimentoRaquete && 
+    if (xBolinha - raio < xRaquete + comprimentoRaquete && 
         yBolinha > yRaquete && 
-        yBolinha < yRaquete + larguraRaquete 
-    ) {
+        yBolinha < yRaquete + larguraRaquete) {
         vxBolinha *= -1; 
     }
+}
+
+function incluiPlacar() {
+    fill(255);
+    textSize(32);
+    text(meusPontos, 278, 50); 
+    text(pontosDoOponente, 321, 50);
+}
+
+function resetBolinha() {
+    xBolinha = width / 2;
+    yBolinha = height / 2;
+    vxBolinha = -vxBolinha; 
+    vyBolinha = 5;
 }
